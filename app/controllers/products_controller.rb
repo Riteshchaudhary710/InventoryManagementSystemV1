638a4612cd6bcd0934211312f111d8358344553e
Riteshchaudhary710 <chaudharyritesh710@gmail.com>
2022-03-14 22:18:33 +0545

    feat: add create adnd index action to the product controllers.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,9 @@
 # products controller for product
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
   def index
-    @products = Product.order('created_at DESC')
-    # render json: { status: 'Success', message: 'products loaded', data: products }, status: :ok
-  end
-
-  def show
-    product = Product.find_by(id: params[:id])
-    render json: { status: 'Success', message: 'product loaded', data: product }, status: :ok
+    products = Product.order('created_at DESC')
+    render json: { status: 'Success', message: 'products loaded', data: products }, status: :ok
   end
 
   def create
@@ -22,26 +17,9 @@ class ProductsController < ApplicationController
     end
   end
 
-  def destroy
-    product = Product.find(params[:id])
-    product.destroy
-    render json: { status: 'sucess', message: 'Deleted Product', data: product }, status: :ok
-  end
-
-  def update
-    product = Product.find(params[:id])
-
-    if product.update(product_params)
-      render json: { status: 'Success', message: 'Product update', data: product }, status: :ok
-    else
-      render json: { status: 'Error', message: 'Product not update', data: product.errors },
-             status: :unprocessable_entity
-    end
-  end
-
   private
 
   def product_params
-    params.permit(:name, :upc)
+    params.permit(:name, :upc, :part_number, :label)
   end
 end
